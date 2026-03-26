a1ca89e8c8d10cfdada889516ddf2906132dd69b
Jonathan <rockyroller2005@gmail.com>
2026-03-26 09:38:45 -0400

    * main.cpp file complete, added a guide on how to use program as well as program purpose statement, and some commands for the user.

--- DateClass/Main.cpp
+++ DateClass/Main.cpp
@@ -1,5 +1,5 @@
 // Author: Jonathan Lopez
-// Program Purpose: 
+// Program Purpose: To collect a day, month, year, check if it is a leap year and to display the day/month/year in three formats.
 
 #include <iostream>
 #include "DateSpecification.h"
@@ -8,11 +8,35 @@ using namespace std;
 
 int main(){
 
+    // ------------------------------- ]
+    // For Reader:                     ]
+    // To create a new date,           ]
+    // create a new object, pass       ]
+    // day, month, year to params      ]
+    // and use .printReport() memeber  ]
+    // function to output your date in ]
+    // 3 formats.                      ]
+    //                                 ]
+    // USABLE METHODS:                 ]
+    // getMonth()                      ]
+    // getDay()                        ]
+    // getYear()                       ]
+    // isLeapYear() 1 = true, 0 = false]
+    // lastDay()                       ]
+    // ------------------------------- ]
 
-        
 
 
-    
+    // Set Date via Constructor
+    Date date1(12, 30, 2020);
+    // print in three formats
+    date1.printReport();
 
-   
+    // Note, when you call any of these member functions it only returns a value, since I didn't implement a cout besides in printReport,  you must use cout. Also must end line.
+    cout << endl;
+    cout << date1.getMonth() << endl;
+    cout << date1.getDay() << endl;
+    cout << date1.getYear() << endl;
+    cout << date1.isLeapYear() << endl;
+    cout << date1.lastDay();
 }
